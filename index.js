b2dd67b2d592c4b59a5742a951b4dd7cda8ccde1
deciob <decio.battaglia@featurespace.co.uk>
2019-06-03 20:51:06 +0100

    added colour picker

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+// http://paperjs.org
+//
+// Exercise:
+// Add a brush width picker, in a similar whay the brush colour picker is implemented.
+// Use: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+
+let brushColor = "black";
+let brushWidth = 3;
+
+function handleBrushColorChange(e) {
+  // Check the event structure:
+  // console.log(e);
+  brushColor = e.target.value;
+}
+
 window.onload = function() {
   // Get a reference to the canvas object
-  var canvas = document.getElementById("draw-area");
+  const canvas = document.getElementById("draw-area");
+
+  // See: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+  const brushColorPicker = document.querySelector("#color-picker");
+  // Check if you have selected something:
+  // console.log(brushColorPicker);
+
+  // See: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+  brushColorPicker.addEventListener(
+    "change" /*this is the event type*/,
+    handleBrushColorChange /*this is the callback function, that gets called every time the 'change' event gets fired*/
+  );
 
   // Create an empty project and a view for the canvas:
   paper.setup(canvas);
@@ -9,7 +35,9 @@ window.onload = function() {
 
   tool.onMouseDown = function(event) {
     path = new paper.Path();
-    path.strokeColor = 'black';
+    // See path methods: http://paperjs.org/reference/path/
+    path.strokeColor = brushColor;
+    path.strokeWidth = brushWidth;
   }
 
   tool.onMouseDrag = function(event) {
